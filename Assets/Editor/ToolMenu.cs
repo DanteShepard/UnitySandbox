48c8047c113c0531c1c39ec9af2c8728326e53a6
Dmitry Stryuk <danshepard.ds@gmail.com>
2018-12-26 15:30:21 +0200

    Add scripts for build on TeamCity CI.

---
+++ Assets/Editor/ToolMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuItems
+{
+    [MenuItem("Tools/Build")]
+    private static void NewMenuOption()
+    {
+        Nordeus.Build.Builder.BuildAndroid("D://alfa_apk.");
+    }
+
+}
